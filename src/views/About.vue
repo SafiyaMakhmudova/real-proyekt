f244608f767ccb383489a4883039f4e5a188c264
Ruxsora <maxmudovarruxsora@gmail.com>
2024-03-05 10:02:13 +0500

    added responsive

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- Modal 1 -->
   <div class="text-left relative mb-[200px]">
-    <div class="bg-[#FFE7B7] pt-[83px] pl-[240px] pb-[180px] pr-[124px]">
-      <h2 class="text-text text-3xl mb-5 font-semibold">What we can offer your SaaS Business</h2>
-      <p class="text-[#665C49] text-xl font-medium">
+    <div class="bg-[#FFE7B7] pt-[83px] pl-[240px]  modal1 pb-[180px] pr-[124px]">
+      <h2 class="text-text text-3xl mb-5 font-semibold modal1h2 ">What we can offer your SaaS Business</h2>
+      <p class="text-[#665C49] text-xl font-medium modal1p">
         We have chosen to only collaborate and work with SaaS businesses, this is because our<br />
         expertise lies and we know that we can provide the most value to our clients. Our team
         has<br />
@@ -16,9 +16,9 @@
   <div>
     <!-- Modal 2 -->
     <div
-      class="bg-white text-center rounded-3xl absolute top-[60%] w-[90%] left-1/2 transform -translate-x-1/2"
+      class="bg-white text-center rounded-3xl modal2 absolute top-[60%] w-[90%] left-1/2 transform -translate-x-1/2"
     >
-      <h2 class="text-text text-3xl font-semibold pt-[64px]">
+      <h2 class="text-text text-3xl font-semibold pt-[64px] modal2h2">
         Services we can offer to our customers
       </h2>
       <p class="pt-[42px] text-xl text-[#1D1D1D]">
@@ -109,11 +109,11 @@
   </div>
 
   <!-- Modal 3 -->
-  <div class="text-left px-[80px] pb-[89px]">
-    <h2 class="text-3xl text-text pb-[56px] font-semibold">VIP books uz history</h2>
-    <div class="flex">
-      <div class="w-[50%]">
-        <span class="text-[#1D1D1D] text-xl"
+  <div class="text-left px-[80px] pb-[89px] modal3">
+    <h2 class="text-3xl text-text pb-[56px] font-semibold modal3h2 ">VIP books uz history</h2>
+    <div class="flex modal3div">
+      <div class="w-[50%] modal3w">
+        <span class="text-[#1D1D1D] text-xl "
           >Every SaaS website, regardless of niche, must do one thing brilliantly, which is
           converting visitors into users. At first glance, the site should encourage and guide
           visitors in a smooth way towards call-to-actions. <br> This goes hand in hand with a responsive
@@ -123,13 +123,114 @@
           be search engine optimized (SEO) because it lays the foundation for the technical quality,
         </span>
       </div>
-      <div class="w-[50%]">
+      <div class="w-[50%] modal3img">
         <img src="../assets/img/1 51.png" alt="">
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
 
-<style lang="js" scoped></style>
+
+</script>
+
+<style scoped>
+@media only screen and (max-width: 768px) {
+  .modal1 {
+    width: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+
+  }
+  .modal1h2 {
+    width: 100%;
+    font-size:x-large ;
+  }
+
+  .modal2 {
+    top: 90%;
+    margin-bottom: 45px;
+    
+  }
+  .modal2h2 {
+    width: 100%;
+    font-size:x-large ;
+  }
+
+  .modal3 {
+    padding: 10px;
+  }
+
+  .modal3h2 {
+    text-align: center;
+    width: 100%;
+    padding-bottom: 10px;
+  }
+  
+  .modal3div {
+    width: 100%;
+    display: block;
+  }
+
+  
+  .modal3w {
+    width: 100%;
+  }
+
+  
+  .modal3img {
+    margin-top: 15px;
+    width: 100%;
+  }
+}
+
+@media only screen and (max-width: 932px) {
+  .modal1 {
+    width: 100%;
+    padding-left: 20px;
+    padding-right: 20px;
+
+  }
+  .modal1h2 {
+    width: 100%;
+    font-size:x-large ;
+  }
+
+  .modal2 {
+    top: 70%;
+    margin-bottom: 45px;
+    
+  }
+  .modal2h2 {
+    width: 100%;
+    font-size:x-large ;
+  }
+
+  .modal3 {
+    padding: 20px;
+  }
+
+  .modal3h2 {
+    text-align: center;
+    width: 100%;
+    padding-bottom: 10px;
+  }
+  
+  .modal3div {
+    width: 100%;
+    display: block;
+  }
+
+  
+  .modal3w {
+    width: 100%;
+  }
+
+  
+  .modal3img {
+    margin-top: 15px;
+    width: 100%;
+  }
+}
+</style>
